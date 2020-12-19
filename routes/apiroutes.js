90a4355bbe5c42987a1236b65d1211ff4dbd238a
JordonMoses <jordon@blackbirdrevolt.com>
2020-12-18 22:59:14 -0600

    Functioning delete

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -2,10 +2,10 @@ const fs = require('fs');
 
 
 
-module.exports = function(application) {
-    application.get("/api/notes", function(req, res){
+module.exports = function (application) {
+    application.get("/api/notes", function (req, res) {
 
-        fs.readFile("./db/db.json", function(err, data) {
+        fs.readFile("./db/db.json", function (err, data) {
             if (err)
                 throw err;
             res.send(JSON.parse(data));
@@ -13,33 +13,54 @@ module.exports = function(application) {
     });
 
 
-    application.post("/api/notes", function(req, res) {
-        
-        fs.readFile("./db/db.json", function(err, data){
+    application.post("/api/notes", function (req, res) {
+
+        fs.readFile("./db/db.json", function (err, data) {
             if (err)
-            throw err;
+                throw err;
             notesss = JSON.parse(data);
             notesss.push(req.body);
-            for (let i = 0; i < notesss.length; i++){
+            for (let i = 0; i < notesss.length; i++) {
                 notesss[i].id = i
             }
-            console.log(notesss);
-            
+
             fs.writeFile("./db/db.json",
-            JSON.stringify(notesss),
-            function(err, data){
-                if (err) {
-                    console.log(err);
-                } else {
-                    res.send(notesss);
+                JSON.stringify(notesss),
+                function (err, data) {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        res.send(notesss);
+                    }
                 }
-            }
             )
-            
+
         });
     });
 
-    
+
+    application.delete("/api/notes/:id", function (req, res) {
+        fs.readFile("./db/db.json", function (err, data) {
+            // if (err) console.log(err);
+            let notesss = JSON.parse(data);
+            const notesssArray = notesss.filter(
+                (item) => item.id !== parseInt(req.params.id)
+            );
+            fs.writeFile(
+                "./db/db.json",
+                JSON.stringify(notesssArray),
+                function (err, data) {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        res.send(notesss);
+                    }
+                }
+            );
+        });
+    });
+
+
 
 
 
