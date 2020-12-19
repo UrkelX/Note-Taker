5e5cef7cbf25c9719dd9adcb867f22e73db1439e
JordonMoses <jordon@blackbirdrevolt.com>
2020-12-18 20:10:23 -0600

    application tupos fixed

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -2,13 +2,13 @@ const path = require("path");
 
 // HTML Route
 module.exports = function (application) {
-    app.get("/notes", function(req, res) {
-        res.sendFile(path.join(__dirname, "./public/notes.html"));
+    application.get("/notes", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/notes.html"));
     });
 
     //GET * will return the index.html file
-    app.get("*", function(req, res) {
-        res.sendFile(path.join(__dirname, "./public/index.html"));
+    application.get("*", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/index.html"));
     });
 };
 
